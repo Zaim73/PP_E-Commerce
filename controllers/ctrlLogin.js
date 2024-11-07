443997bc6de78b10144e17c823538be889148501
syabinaaisyah <syabinaaisyahputri.mipa3@gmail.com>
2024-11-08 02:42:38 +0700

    Syabina

--- controllers/ctrlLogin.js
+++ controllers/ctrlLogin.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcryptjs");
 
 exports.home = async (req, res) => {
   try {
-    res.render("login");
+    res.render("home");
   } catch (error) {
     console.log("🚀 ~ exports.home= ~ error:", error);
     res.send(error.message);
@@ -12,7 +12,7 @@ exports.home = async (req, res) => {
 
 exports.login = async (req, res) => {
   try {
-    res.render("login");
+    res.render("home");
   } catch (error) {
     console.log("🚀 ~ exports.login= ~ error:", error);
     res.send(error.message);
